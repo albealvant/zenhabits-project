bf5f80fea994a5fe6f362d0bc59bd042e2d251e4
albafam25 <alba.almoril@famytec.com>
2025-05-20 16:50:45 +0200

    Set up db connection

--- zenhabits_server/src/main.rs
+++ zenhabits_server/src/main.rs
@@ -1,21 +1,27 @@
 use axum::{
-    routing::{get, post},
-    Router,
+    routing::{get, post}, Router
 };
 use dotenv::dotenv;
+
+pub mod routes;
+pub mod models;
+pub mod db;
+use crate::db::get_db_connection;
+use crate::routes::check_handler::check;
+use crate::routes::save_data_handler::save_data;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    let pool = get_db_connection().await.unwrap();
+
+    let app = Router::new()
+        .route("/", get(check))
+        .route("/save", post(save_data))
+        .with_state(pool);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-    let app = Router::new().route("/hello", get(hello));
-
     println!("Servidor en ejecucion: http://0.0.0.0:3000/");
 
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn hello() -> &'static str {
-    "bye\n"
 }
