38fecbf071d89c441f50970ef74eb40c371a580d
albafam25 <alba.almoril@famytec.com>
2025-05-20 10:24:32 +0200

    Add gitignore. Init rust

---
+++ zenhabits_server/target/debug/deps/zenhabits_server-7b9540bb45ce9123.d
@@ -0,0 +1,5 @@
+/home/albafam25/code/zenhabits-project/zenhabits_server/target/debug/deps/libzenhabits_server-7b9540bb45ce9123.rmeta: src/main.rs
+
+/home/albafam25/code/zenhabits-project/zenhabits_server/target/debug/deps/zenhabits_server-7b9540bb45ce9123.d: src/main.rs
+
+src/main.rs:
