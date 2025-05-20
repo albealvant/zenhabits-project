baaefef201536535607ac67a180c8b0fcf68c34a
albafam25 <alba.almoril@famytec.com>
2025-05-20 10:45:29 +0200

    Basic API structure + fix gitignore

--- zenhabits_server/src/main.rs
+++ zenhabits_server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    let app = Router::new().route("/hello", get(hello));
+
+    println!("Servidor en ejecucion: http://0.0.0.0:3000/");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "bye\n"
 }
--- zenhabits_server/target/debug/deps/zenhabits_server-57878c45f9d05e9e.d
+++
@@ -1,5 +0,0 @@
-/home/albafam25/code/zenhabits-project/zenhabits_server/target/debug/deps/libzenhabits_server-57878c45f9d05e9e.rmeta: src/main.rs
-
-/home/albafam25/code/zenhabits-project/zenhabits_server/target/debug/deps/zenhabits_server-57878c45f9d05e9e.d: src/main.rs
-
-src/main.rs:
